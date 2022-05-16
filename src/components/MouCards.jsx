0f82a2ee77a90dc02215ff74b06ba2dcb1ac57d3
kg-kartik <goel.kartik39@gmail.com>
2022-05-17 00:21:29 +0530

    Made connections for cards and update status

--- src/components/MouCards.jsx
+++ src/components/MouCards.jsx
@@ -1,19 +1,57 @@
 import React from "react";
+import Axios from "axios";
 
 const MouCard = ({
-	name,
+	mouWith,
     file,
+	_id,
+	user,
     purpose,
-    assignee
+    assignee,
+	btn
 }) => {
+
+	const statusUpdate = (status) => {
+		Axios.post("http://localhost:5000/mou/updateStatus",{
+			status,
+			mouId:_id,
+		},{
+			headers:{
+				"Authorization":"Bearer "+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjRmNDY0OGY4M2VhYjRhYWFhNzgzNWEiLCJpYXQiOjE2NTI3MDQzNTh9.ccsPBXshG4KG6aTWS2aVxVNyrIeb-hPFe7KNGNTZD3c"
+			}
+		}).then((data) => {
+			console.log(data);
+			window.alert("Status updated");
+		}).catch((err) => {
+			console.log(err);
+		})
+	}
+
 	return (
 		<div className="EventCard">
-			<div className="name">{name}</div>
+			<div className="name">{mouWith}</div>
 			<div className="details">
-				<div className="type">{assignee}</div>
-				<div className="date">{file}</div>
+				<div className="type">{user.name}</div>
+				{/* <div className="date">{file}</div> */}
 			</div>
 			<div className="description">{purpose.substring(0, 80)}...</div>
+			{btn ? (
+				<div className="status-buttons">
+					<button
+						onClick={() => statusUpdate("Accepted")}
+						className="btn btn-accent">
+						Accept
+					</button>
+					<button
+						onClick={() => statusUpdate("Rejected")}
+						className="btn btn-accent">
+						Reject
+					</button>
+				</div>
+			) :(
+				<>
+				</>
+			)}
 		</div>
 	);
 };
--- src/pages/Mous.jsx
+++ src/pages/Mous.jsx
@@ -1,41 +1,74 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import MouCard from "../components/MouCards";
+import Axios from "axios";
 
 const Mous = () => {
 	
-    const mous = [{
-        name:"lol",
-        purpose:"Internship program to offer internship to 23 batch students for full stack dev roles",
-        assignee:"Mr Ramesh",
-        file:"lol"
-    },
-    {
-        name:"lol",
-        purpose:"Internship program to offer internship to 23 batch students for full stack dev roles",
-        assignee:"Mr Ramesh",
-        file:"lol"
-    },
-    {
-        name:"lol",
-        purpose:"Internship program to offer internship to 23 batch students for full stack dev roles",
-        assignee:"Mr Ramesh",
-        file:"lol"
-    },
-    {
-        name:"lol",
-        purpose:"Internship program to offer internship to 23 batch students for full stack dev roles",
-        assignee:"Mr Ramesh",
-        file:"lol"
-    }
-]
+    // const [mous,setMous] = useState([]);
+    const [acceptedMou,setAcceptedMou] = useState([]);
+    const [rejectedMou,setRejectedMou] = useState([]);
+    const [pendingMou,setPendingMou] = useState([]);
+
+
+    useEffect(() => {
+        Axios.get("http://localhost:5000/mou/getAllMous",{
+			headers:{
+				"Authorization":"Bearer "+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjRmNDY0OGY4M2VhYjRhYWFhNzgzNWEiLCJpYXQiOjE2NTI3MDQzNTh9.ccsPBXshG4KG6aTWS2aVxVNyrIeb-hPFe7KNGNTZD3c"
+			}
+		}).then((res) => {
+            let accepted =[],rejected =[],pending=[];
+			console.log(res.data.data);
+            res.data.data.map((mou) => {
+                if(mou.status === "Accepted"){
+                    console.log(mou,"accepted");
+                    accepted.push(mou);
+                }
+                
+                if(mou.status === "Pending"){
+                    pending.push(mou);
+                }
+                
+                if(mou.status === "Rejected"){
+                    rejected.push(mou);
+                }
+                
+            })
+            setAcceptedMou(accepted);
+            setPendingMou(pending);
+            setRejectedMou(rejected);
+
+		}).catch((err) => {
+			console.log(err,"ERROR");
+		})
+    },[])
 
     return (
 		<div className="EventCards">
 			<div className="container">
 					<div className="heading">MOUs Signed</div>
 				<div className="events">
-                    {mous &&
-                        mous.map((e) => (
+                    {pendingMou &&
+                        pendingMou.map((e) => (
+                            <MouCard btn={true} key={e.name} {...e} />
+                        ))}
+				</div>
+			</div>
+
+            <div className="container">
+					<div className="heading">MOUs Accepted</div>
+				<div className="events">
+                    {acceptedMou &&
+                        acceptedMou.map((e) => (
+                            <MouCard key={e.name} {...e} />
+                        ))}
+				</div>
+			</div>
+
+            <div className="container">
+					<div className="heading">MOUs Rejected</div>
+				<div className="events">
+                    {rejectedMou &&
+                        rejectedMou.map((e) => (
                             <MouCard key={e.name} {...e} />
                         ))}
 				</div>
